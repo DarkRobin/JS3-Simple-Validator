494fdd874126a2f30523935732699febe1da5a6b
charles <sam.charles.harrod@gmail.com>
2015-01-19 17:01:45 -0700

    first commit help

---
+++ main.js
@@ -0,0 +1,12 @@
+var userInput = prompt("What is your phone number? Rest ", "   -   -");
+var phoneNumber = 13;
+	if (phoneNumber === userInput.length){
+		alert("Thank You for your nmber");	
+
+
+
+
+
+	}
+
+
